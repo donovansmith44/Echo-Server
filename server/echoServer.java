ce54daaaa116566e510d0009640d6b5d00ba06fb
donovansmith44 <donovan.smith44@gmail.com>
2022-12-31 18:44:04 -0500

    initial commit

---
+++ server/echoServer.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+class echoServer{
+    //public static void main(String[] args) {
+    void run(){
+        System.out.println("Echo server");
+
+        try (ServerSocket echoServer = new ServerSocket(9999)){
+
+            System.out.println("Waiting for connection...");
+
+            Socket clientSocket = echoServer.accept();
+
+            System.out.println("Conneted to client!");
+
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)){
+
+                String message;
+
+                while((message = reader.readLine()) != null)
+                {
+                    System.out.println("Server: " + message);
+                    out.println(message);
+                }
+                
+                }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+}
